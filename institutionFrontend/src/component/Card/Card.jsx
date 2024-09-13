62b43f7385c7c82fc0c1978e633162d49f3679b0
mohammedanaz <mohammedanaz@gmail.com>
2024-09-13 23:16:29 +0530

    #33 all programes page integrated with backend successfully. by Anaz

--- institutionFrontend/src/component/Card/Card.jsx
+++ institutionFrontend/src/component/Card/Card.jsx
@@ -1,7 +1,7 @@
 import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-const CommonCard = ({ title, duration, price, image, link }) => {
+const CommonCard = ({ name, duration, price, image, link }) => {
   const navigate = useNavigate();
 
   const handleCardClick = () => {
@@ -13,7 +13,7 @@ const CommonCard = ({ title, duration, price, image, link }) => {
   return (
     <Card sx={{ 
         maxWidth: 345, 
-        minHeight: 310,
+        height: '100%',
         display: 'flex', 
         flexDirection: 'column', 
         margin: 'auto', 
@@ -26,11 +26,13 @@ const CommonCard = ({ title, duration, price, image, link }) => {
       onClick={handleCardClick}
       >
       <CardMedia>
-        <img src="https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2023/02/how-to-create-online-course.webp" alt="Course-image"  sx={{ height: 140, objectFit: 'cover' }}/>
+        <img src={image} alt={`${name}-image`}  sx={{ height: 140, objectFit: 'cover' }}/>
       </CardMedia>
       <CardContent>
-        <Typography variant="h6" component="div">
-          {title}
+        <Typography variant="h6" component="div"
+        sx={{ minHeight: 70 }}
+        >
+          {name}
         </Typography>
         {duration ? (
           <Typography variant="body2" component="div">
