0dae1676b7ec0c412b4b8f0200389224de004d5a
aashiashfak <aashiashfak@gmail.com>
2024-10-19 11:12:06 +0530

    #103v1 completed product filter page

--- institutionFrontend/src/services/user/ProductServices.jsx
+++ institutionFrontend/src/services/user/ProductServices.jsx
@@ -1,10 +1,27 @@
 import instance from "../../utils/axios";
 
 const ProductsServices = {
-  getProducts: async (category) => {
-    const response = await instance.get(`store/list-create/${category}`);
+  getProducts: async (category, filterOptions = {}) => {
+    console.log('filterOptions-------',filterOptions)
+    const params = {
+      sub_category: filterOptions.sub_category?.length
+        ? filterOptions.sub_category.join(",")
+        : "",
+      min_price: filterOptions.min_price || "",
+      max_price: filterOptions.max_price || "",
+      color: filterOptions.color || "",
+      size: filterOptions.size || "",
+      is_active: filterOptions.is_active || "",
+    };
+    const response = await instance.get(`shop-admin/list-create/${category}`, {
+      params: params,
+    });
+    return response.data;
+  },
+  getSubcategories: async () => {
+    const response = await instance.get("store/list-sub-categories");
     return response.data;
   },
 };
 
-export default ProductsServices
+export default ProductsServices;
